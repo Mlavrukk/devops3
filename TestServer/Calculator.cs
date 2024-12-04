e97753e91c7bfe5241fc4204cf6b93ad696bada0
unknown <margolav27@yandex.ru>
2024-12-04 15:57:28 +0400

    init

---
+++ TestServer/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using TestServer.Model;
+
+namespace TestServer;
+
+public static class Calculator
+{
+    public static void Hello() {
+        return;
+    }
+    
+    public static int Sum(IEnumerable<Number> numbers) => numbers.Sum(n => n.Value);
+}
